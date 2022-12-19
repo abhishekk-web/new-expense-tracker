4de6289d834cffa28395edb76cde3238a274b5a0
abhishekk-web <abhi.chatterjee38@gmail.com>
2022-12-19 11:17:55 -0800

    Sign up page of expense tracker

---
+++ signup.js
@@ -0,0 +1,35 @@
+async function signup(e){
+
+    try {
+
+        e.preventDefault();
+        console.log("hello world", e.target.name.value);
+
+        const signupDetails = {
+            name: e.target.name.value,
+            email: e.target.email.value,
+            password: e.target.password.value
+        }
+        console.log(signupDetails);
+
+        const response = await axios.post("http://localhost:3000/user/signup", signupDetails);
+
+        if(response.status === 201){
+
+            console.log("login");
+
+        }
+        else{
+            throw new Error('Failed to login');
+        }
+        
+    }
+
+    catch(err) {
+
+        document.body.innerHTML += `<div style="color: red">${err}</div>`;
+        console.log(err);
+
+    }
+
+}
