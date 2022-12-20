ad77bb29b872dcb41b060a9d95a16fe7371969af
abhishekk-web <abhi.chatterjee38@gmail.com>
2022-12-20 13:38:03 -0800

    Adding login page into the frontend

---
+++ login.js
@@ -0,0 +1,22 @@
+ async function login(e) {
+    try{
+    e.preventDefault();
+    console.log(e.target.email.value);
+
+    const loginDetails = {
+
+        email: e.target.email.value,
+        password: e.target.password.value
+
+    }
+
+    console.log(loginDetails);
+    const response = await axios.post('http://localhost:4000/login', loginDetails)
+            alert(response.data.message);
+    }
+    catch(err) {
+        console.log(JSON.stringify(err));
+        document.body.innerHTML += `<div style="color: red;">${err}</div>`
+    }
+
+}
